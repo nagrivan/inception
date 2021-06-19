bc57d3f1bdfd25f2f255da83a4e27867f5267b3c
Neucar Agrivane <nagrivan@pl-k1.msk.21-school.ru>
2021-06-19 19:13:15 +0300

    create user admin in Maria DB

--- srcs/requirements/mariadb/tools/starting_mariadb.sh
+++ srcs/requirements/mariadb/tools/starting_mariadb.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+service mysql start
+echo "Service Maria DB is starting..."
+mysql
+CREATE DATABASE IF NOT EXISTS wordpress;
+USE wordpress
+CREATE USER 'nagrivan'@ IDENTIFIED BY 'FuckingPassword';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'nagrivan';
+FLUSH PRIVILEGES;
